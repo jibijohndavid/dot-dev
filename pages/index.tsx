0c207ad4924aa435568cbe943a20707c7eb6472a
Jibi John David <jibijohndavid@gmail.com>
2021-08-03 22:37:02 +0530

    Added Tailwind v2.2.7

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
-        <h1>JIBI DOT DEV</h1>
+    <div className="flex items-center justify-center h-screen bg-black">
+      <h1 className="text-3xl font-bold text-gray-300">JIBI DOT DEV</h1>
     </div>
-  )
+  );
 }
