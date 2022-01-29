50edb0b616c068273e6259c2d1042490ad7df19a
Jibi John David <jibijohndavid@gmail.com>
2022-01-30 02:20:03 +0530

    Updated about me text contents

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -6,16 +6,16 @@ const skills = [
   "React",
   "React Native",
   "NextJS",
-  "ES6+",
   "Typescript",
-  "Node",
+  "NodeJS",
   "Express",
   "PostgreSQL",
   "MongoDB",
+  "Azure",
 ];
 
 const Tag = ({ title }: { title: string }) => (
-  <span className="mr-4 mb-4 px-2 py-1 text-base rounded text-white  bg-purple-600 font-medium opacity-80">
+  <span className="mr-4 mb-4 px-2 py-1 text-base rounded text-white  bg-purple-600 font-medium opacity-70">
     {title}
   </span>
 );
@@ -24,34 +24,35 @@ const AboutMe = () => {
   return (
     <section>
       <UIContainer>
-        <div className="container py-20 mx-auto">
+        <div className="container my-20 mx-auto">
           <div className="items-center lg:flex">
             <div className="lg:w-2/3">
               <h2 className="text-3xl font-bold text-indigo-400">About me</h2>
-              <p className="my-4 text-gray-500 lg:max-w-xl">
-                I am Jibi John David, a passionate&nbsp;
-                <span className="font-bold text-indigo-400">
+              <div className="my-4 text-base text-gray-500 lg:max-w-2xl">
+                My name is <strong>Jibi John David</strong>. I am a
+                passionate&nbsp;
+                <strong className="font-bold text-indigo-400">
                   Software Engineer&nbsp;
-                </span>
+                </strong>
                 having 5+ years of industry experience in analysis, design and
                 development of Web and Mobile based solutions.
                 <p className="mt-2">
-                  Motivated by vision and inspiration, focusing on ideas and
-                  concepts and turning them into digital experiences.
+                  I am motivated by vision and inspiration. I focus on ideas and
+                  concepts. Having said that I find joy in turning them into
+                  digital experiences.
                 </p>
                 <p className="mt-2">
-                  Currently I am working with&nbsp;
+                  Presently I am working with&nbsp;
                   <Link href="https://www.pitsolutions.ch/en/" replace>
                     <a target="_blank" className="font-bold underline">
                       PIT Solutions
                     </a>
                   </Link>
-                  , leading a team of talented engineers and mentoring them to
-                  their full potential while working closely with the
-                  architecture team for the design and delivery of the project
-                  features.
+                  , leading a team of talented engineers and also mentoring them
+                  to their full potential while working closely with other
+                  stakeholders for the design and delivery of the projects.
                 </p>
-              </p>
+              </div>
 
               <div className="mt-8 lg:max-w-xl flex flex-wrap">
                 {skills.map((skill) => (
