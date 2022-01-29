74e0c5bca9f8da2eaf65cd6f7b1909f901485336
Jibi John David <jibijohndavid@gmail.com>
2022-01-29 19:15:32 +0530

    removed nav from _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Footer from "../components/Footer";
-import Nav from "../components/Nav";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Nav />
         <Head></Head>
         <body className="bg-dark">
           <Main />
